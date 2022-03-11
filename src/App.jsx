3af3149ff1e361e0f03523083e9efa476c93b053
fiftysev <nesgorai@bk.ru>
2022-03-11 17:33:13 +1000

    Create basic layout with data from api

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  const [data, setData] = useState();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async () => {
+      await fetch("https://reqres.in/api/users/2")
+        .then(res => res.json())
+        .then(v => {
+          setData(v.data);
+          setLoading(false);
+        });
+    }
+
+    getUser();
+  }, []);
+
+  return (
+    <div className="App-wrapper">
+      <header className="header">
+        <img src={logo} alt="" />
+      </header>
+      <nav className="nav">
+        <a href="#">Profile</a>
+        <a href="#">Messages</a>
+        <a href="#">News</a>
+        <a href="#">Music</a>
+        <a href="#">Settings</a>
+      </nav>
+      <main className="content">
+        {!isLoading && <div className="profile">
+          <img src={`${data.avatar}`} alt="asdfsdf" className="avatar" />
+          <div className="info">
+              <p>{data.first_name} {data.last_name}</p>
+              <p>{data.email}</p>
+          </div>
+        </div>}
+      </main>
+    </div>
+  );
+}
+
+export default App;
