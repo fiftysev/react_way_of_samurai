9686067bf7c1f7570a388eebfc12f53be90de844
fiftysev <nesgorai@bk.ru>
2022-03-11 18:32:41 +1000

    Add prettier and reformat code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { useRef } from "react";
 
 const App = () => {
   const [data, setData] = useState();
@@ -11,21 +12,19 @@ const App = () => {
   useEffect(() => {
     const getUser = async () => {
       await fetch("https://reqres.in/api/users/2")
-        .then(res => res.json())
-        .then(v => {
+        .then((res) => res.json())
+        .then((v) => {
           setData(v.data);
           setLoading(false);
         });
-    }
+    };
 
     getUser();
   }, []);
 
   return (
     <div className="App-wrapper">
-      <header className="header">
-        <img src={logo} alt="" />
-      </header>
+      <Header />
       <nav className="nav">
         <a href="#">Profile</a>
         <a href="#">Messages</a>
@@ -34,16 +33,20 @@ const App = () => {
         <a href="#">Settings</a>
       </nav>
       <main className="content">
-        {!isLoading && <div className="profile">
-          <img src={`${data.avatar}`} alt="asdfsdf" className="avatar" />
-          <div className="info">
-              <p>{data.first_name} {data.last_name}</p>
+        {!isLoading && (
+          <div className="profile">
+            <img src={`${data.avatar}`} alt="asdfsdf" className="avatar" />
+            <div className="info">
+              <p>
+                {data.first_name} {data.last_name}
+              </p>
               <p>{data.email}</p>
+            </div>
           </div>
-        </div>}
+        )}
       </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="" />
+    </header>
+  );
+};
+
+export default Header;
