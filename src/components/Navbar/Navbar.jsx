16be3871551ec813530b5c147d1c95e1f95727f4
fiftysev <nesgorai@bk.ru>
2022-03-14 12:49:48 +1000

    Normalize links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <a href="/profile">Profile</a>
-      <a href="/dialogs">Messages</a>
-      <a href="#news">News</a>
-      <a href="#music">Music</a>
-      <a href="#settings">Settings</a>
+      <NavLink
+        to="/profile"
+        className={(navData) => (navData.isActive ? s.active : "")}>
+        Profile
+      </NavLink>
+      <NavLink
+        to="/dialogs"
+        className={(navData) => (navData.isActive ? s.active : "")}>
+        Messages
+      </NavLink>
+      <NavLink
+        to="/news"
+        className={(navData) => (navData.isActive ? s.active : "")}>
+        News
+      </NavLink>
+      <NavLink
+        to="/music"
+        className={(navData) => (navData.isActive ? s.active : "")}>
+        Music
+      </NavLink>
+      <NavLink
+        to="/settings"
+        className={(navData) => (navData.isActive ? s.active : "")}>
+        Settings
+      </NavLink>
     </nav>
   );
 };
