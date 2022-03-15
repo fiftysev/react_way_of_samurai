6fdf41cd2dcfe3cf38971b778c9b1979b703016e
fiftysev <nesgorai@bk.ru>
2022-03-15 16:57:32 +1000

    minor fix

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -4,18 +4,27 @@ import s from "./posts.module.css";
 import { v4 } from "uuid";
 
 const Posts = (props) => {
+  let newPost = React.createRef();
+
+  let addPost = () => {
+    let text = newPost.current.value;
+    alert(text);
+  };
   return (
     <div className={s.container}>
       <h2 className={s.title}>My posts</h2>
       <div className={s.post__form}>
         <textarea
+          ref={newPost}
           className={s.form__input}
           name="postText"
           id="create_post"
           cols="20"
           rows="2"
         />
-        <button className={s.form__button}>Add post</button>
+        <button className={s.form__button} onClick={addPost}>
+          Add post
+        </button>
       </div>
       {props.posts.map((v) => (
         <Post text={v.text} likes={v.likes} key={v4()} />
