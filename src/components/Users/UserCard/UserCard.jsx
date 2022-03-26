e489411a468759980e6759857671c806dd7919bb
fiftysev <nesgorai@bk.ru>
2022-03-26 23:18:07 +1000

    Add icons

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import s from "./user_card.module.css";
+import { SiHomeassistantcommunitystore } from "react-icons/si";
+import { BsChatTextFill } from "react-icons/bs";
 
 const UserCard = ({ user, follow, unfollow }) => {
   return (
     <div className={s.container}>
       <img src="./IMG_4421.JPG" alt="avatar" className={s.avatar} />
-      <p className={s.text}>
+      <p className={s.text + " " + s.name}>
         {user.first_name} {user.last_name}
       </p>
       <p className={s.text}>
-        {user.address.city}, {user.address.country}
+        <SiHomeassistantcommunitystore /> {user.address.city},{" "}
+        {user.address.country}
       </p>
-      <p className={s.text}>{user.status}</p>
+      <p className={s.text}>
+        <BsChatTextFill /> {user.status}
+      </p>
       <p className={s.text}>@{user.username}</p>
       {!user.followed && (
         <button
