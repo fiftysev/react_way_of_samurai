2655fd3413fac2454e69970c5463dc4a30d19792
fiftysev <nesgorai@bk.ru>
2022-03-31 15:35:52 +1000

    Minor fix

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,38 +2,26 @@ import React from "react";
 import s from "./profile.module.css";
 
 const ProfileInfo = ({ user }) => {
-  if (user) {
-    return (
-      <div className={s.wrapper}>
-        <div className={s.profile}>
-          <img src={user.photos.small} alt="avatar" className={s.avatar} />
-          <div className={s.info}>
-            <p className={s.info__text}>{user.fullName}</p>
-            <p className={s.info__text}>
-              {user.lookingForAJob && "Ищу работу"}
-            </p>
-            <p className={s.info__text}>{user.lookingForAJobDescription}</p>
-          </div>
+  return (
+    <div className={s.wrapper}>
+      <div className={s.profile}>
+        <img
+          src={
+            user?.photos.small ?? "http://source.unsplash.com/random/240x240"
+          }
+          alt="avatar"
+          className={s.avatar}
+        />
+        <div className={s.info}>
+          <p className={s.info__text}>{user?.fullName ?? "Janet Weaver"}</p>
+          <p className={s.info__text}>{user?.lookingForAJob && "Ищу работу"}</p>
+          <p className={s.info__text}>
+            {user?.lookingForAJob && user.lookingForAJobDescription}
+          </p>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className={s.wrapper}>
-        <div className={s.profile}>
-          <img
-            src={"https://source.unsplash.com/random/128x128"}
-            alt="avatar"
-            className={s.avatar}
-          />
-          <div className={s.info}>
-            <p className={s.info__text}>Janet Weaver</p>
-            <p className={s.info__text}>janet.weaver@reqres.in</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default ProfileInfo;
