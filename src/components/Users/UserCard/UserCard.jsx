74fb75a87408f242517eff4a77fee3c47c923af8
fiftysev <nesgorai@bk.ru>
2022-04-07 18:04:07 +1000

    Add following progress state flag

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./user_card.module.css";
 import { SiHomeassistantcommunitystore } from "react-icons/si";
 import { BsChatTextFill } from "react-icons/bs";
@@ -6,21 +6,28 @@ import { NavLink } from "react-router-dom";
 import { UserService } from "../../../api/services/userService";
 
 const UserCard = ({ user, ...actions }) => {
+  const [inFollowingProgress, setFollowingProgress] = useState(false);
+
   const avatarSrc = user.photos.small ? user.photos.small : "./IMG_4421.JPG";
+
   const follow = () => {
+    setFollowingProgress(!inFollowingProgress);
     UserService.follow(user.id).then((data) => {
       if (data.resultCode === 0) {
         actions.follow(user.id);
       }
     });
+    setFollowingProgress(!inFollowingProgress);
   };
 
   const unfollow = () => {
+    setFollowingProgress(!inFollowingProgress);
     UserService.unfollow(user.id).then((data) => {
       if (data.resultCode === 0) {
         actions.unfollow(user.id);
       }
     });
+    setFollowingProgress(!inFollowingProgress);
   };
 
   return (
@@ -39,19 +46,14 @@ const UserCard = ({ user, ...actions }) => {
         <BsChatTextFill /> {user.status || "React Status"}
       </p>
       {user.uniqueUrlName && <p className={s.text}>@{user.uniqueUrlName}</p>}
-      {!user.followed && (
-        <button className={s.follow_btn} type="button" onClick={follow}>
-          Follow
-        </button>
-      )}
-      {user.followed && (
-        <button
-          className={s.unfollow_btn}
-          type="button"
-          onClick={() => unfollow(user.id)}>
-          Unfollow
-        </button>
-      )}
+
+      <button
+        className={user.followed ? s.unfollow_btn : s.follow_btn}
+        type="button"
+        onClick={user.followed ? unfollow : follow}
+        disabled={inFollowingProgress}>
+        {user.followed ? "Unfollow" : "Follow"}
+      </button>
     </div>
   );
 };
